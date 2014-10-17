51b34a8c27bdd5c81fc8b2e45ea2afdfa7455a59
nbafrank <fvallani@stanford.edu>
2014-10-16 21:45:20 -0700

    added code

---
+++ R/ggflow.R
@@ -0,0 +1,240 @@
+
+
+
+
+#######################################################################################
+#R Functions
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#######################################################################################
+
+library(flowCore);
+library(ggplot2);
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#ggflow_plot
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#This function plots a flowFrame into a scatter plot. This object is refered to as 
+#a "ggflow" object
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ggflow_plot <- function(flowFrame,
+                   x_value,
+                   y_value,
+                   logx    = TRUE,
+                   logy    = TRUE,
+                   x_lim   = NA,
+                   y_lim   = NA,
+                   contour = TRUE){
+  #extract data
+  value_matrix <- as.data.frame(exprs(flowFrame));
+  
+  #match positions 
+  x_pos <- match(x_value,colnames(value_matrix));
+  y_pos <- match(y_value,colnames(value_matrix));
+  
+  #Create default entry for GGPLOT object
+  ggplot_obj <- NA;
+  
+  #if both matches proceed
+  if(all(!is.na(c(x_pos,y_pos)))){
+    #create input data.frame
+    input_df <- data.frame(x=value_matrix[,x_pos],
+                           y=value_matrix[,y_pos],
+                           value_matrix);
+    
+    #adjsut names of data.frame
+    colnames(input_df) <- c('x','y',colnames(value_matrix));
+    
+    #remove 0 values [change this into-> turn 0s into 1s]
+    input_df$x[which(input_df$x==0)] <- 1;
+    input_df$y[which(input_df$y==0)] <- 1;
+    
+    #create ggplot object
+    ggplot_obj <- ggplot(input_df,aes(x=x,y=y));
+    
+    #introduce log scale if necessary
+    if(logx==TRUE){
+      ggplot_obj <- ggplot_obj + scale_x_log10();
+    }
+    if(logy==TRUE){
+      ggplot_obj <- ggplot_obj + scale_y_log10();
+    }
+    #
+    ggplot_obj <- ggplot_obj                    + 
+      xlab(x_value)   + ylab(y_value)           + 
+      geom_point(size =3,
+                 colour="darkgrey",
+                 alpha=0.9)                     +
+      theme_bw()                                +
+      theme(axis.title.x = element_text(size=20),
+            axis.title.y = element_text(size=20),
+            axis.text.x  = element_text(size=20),
+            axis.text.y  = element_text(size=20));
+    
+    if(contour==TRUE){
+      ggplot_obj <- ggplot_obj                  +
+        stat_density2d(aes(alpha=..level..,
+                           fill =..level..),
+                       contour=TRUE,
+                       size=2,
+                       bins=500,
+                       geom="polygon")          +
+        scale_fill_gradient(low = 'blue',
+                            high= 'red')        +
+        scale_alpha(range = c(0.03, 0.03),
+                    guide = FALSE)              +
+        labs(fill="Density")
+    }
+    
+    #specificy the 
+    if(all(!is.na(x_lim))){
+      #make sure there is enough value
+      if(x_lim[1]==0){x_lim[1] <- 1}
+      
+      ggplot_obj <- ggplot_obj +
+        coord_cartesian(xlim = x_lim); 
+    }
+    if(all(!is.na(y_lim))){
+      #make sure there is enough value
+      if(y_lim[1]==0){y_lim[1] <- 1}
+      
+      ggplot_obj <- ggplot_obj +
+        coord_cartesian(ylim = y_lim); 
+    }
+    if(all(!is.na(y_lim)) && all(!is.na(x_lim))){
+      #make sure there is enough value
+      if(x_lim[1]==0){x_lim[1] <- 1}
+      if(y_lim[1]==0){y_lim[1] <- 1}
+      
+      ggplot_obj <- ggplot_obj +
+        coord_cartesian(xlim = x_lim,
+                        ylim = y_lim); 
+    } 
+  }
+  
+  #return object
+  return(ggplot_obj);
+}
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#gg_rectgater_display
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#this function displays a gate on a ggflow object
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+gg_rectgater_display <- function(gg_flow_plot,
+                                 rectGate = NULL,
+                                 size_seg = 1,
+                                 col_seg  = "orange",
+                                 label    = TRUE,
+                                 col_lab  = "orangered",
+                                 type_seg = 2){
+  
+  if(!is.null(rectGate)){
+    
+    #check if gate dimensions are even in the plot
+    name_map <- match(c(gg_flow_plot$labels$y,gg_flow_plot$labels$x),
+                      names(rectGate@max));
+    
+    #if both x and y match add gate
+    if(all(!is.na(name_map))){
+      
+      #define min and max coordinates
+      x_min <- rectGate@min[name_map[2]];
+      x_max <- rectGate@max[name_map[2]];
+      y_min <- rectGate@min[name_map[1]];
+      y_max <- rectGate@max[name_map[1]];
+      
+      #Calculate numbers [for x and y]
+      x_select <- intersect(which(gg_flow_plot$data$x>=x_min),
+                            which(gg_flow_plot$data$x<=x_max));
+      y_select <- intersect(which(gg_flow_plot$data$y>=y_min),
+                            which(gg_flow_plot$data$y<=y_max));
+      
+      #identify events selected by gating
+      selected_cells <- length(intersect(x_select,y_select));
+      selected_perc  <- selected_cells/nrow(gg_flow_plot$data);
+      
+      #Add gate as segments
+      gg_flow_plot <- gg_flow_plot + 
+        geom_segment(data=data.frame(x    = x_min,
+                                     xend = x_min,
+                                     y    = y_min,
+                                     yend = y_max),
+                     aes(x=x,y=y,xend=xend,yend=yend),
+                     size     = size_seg,
+                     colour   = col_seg,
+                     linetype = type_seg)+
+        geom_segment(data=data.frame(x    = x_min,
+                                     xend = x_max,
+                                     y    = y_min,
+                                     yend = y_min),
+                     aes(x=x,y=y,xend=xend,yend=yend),
+                     size     = size_seg,
+                     colour   = col_seg,
+                     linetype = type_seg)+
+        geom_segment(data=data.frame(x    = x_max,
+                                     xend = x_max,
+                                     y    = y_min,
+                                     yend = y_max),
+                     aes(x=x,y=y,xend=xend,yend=yend),
+                     size     = size_seg,
+                     colour   = col_seg,
+                     linetype = type_seg)+
+        geom_segment(data=data.frame(x    = x_min,
+                                     xend = x_max,
+                                     y    = y_max,
+                                     yend = y_max),
+                     aes(x=x,y=y,xend=xend,yend=yend),
+                     size     = size_seg,
+                     colour   = col_seg,
+                     linetype = type_seg);
+      
+      #display percentage cells
+      if(label==TRUE){
+        gg_flow_plot <- gg_flow_plot + 
+          annotate("text",
+                   label=paste(round(selected_perc*100,2),"%",sep=""),
+                   x=x_min,
+                   y=y_min,
+                   colour= col_lab,
+                   size=8);
+      }
+    }
+  }
+  
+  #return ggplot object
+  return(gg_flow_plot);
+}
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#gg_rectgater_cut
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#this function modifies a ggflow object by gating on certain specific values
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+gg_rectgater_cut <- function(gg_flow_plot,
+                             rectGate=NULL){
+  
+  #check if gate dimensions are even in the plot
+  if(!is.null(rectGate)){
+    name_map <- match(names(rectGate@max),
+                      colnames(gg_flow_plot$data));
+    
+    #if both x and y match add gate
+    if(all(!is.na(name_map))){
+      
+      #select points that match the plot and exclude them
+      set_one <- intersect(which(gg_flow_plot$data[,name_map[1]]>=rectGate@min[1]),
+                           which(gg_flow_plot$data[,name_map[1]]<=rectGate@max[1]));
+      
+      set_two <- intersect(which(gg_flow_plot$data[,name_map[2]]>=rectGate@min[2]),
+                           which(gg_flow_plot$data[,name_map[2]]<=rectGate@max[2]));
+      
+      #modify plot
+      gg_flow_plot$data <- gg_flow_plot$data[intersect(set_one,set_two),];
+    }
+  }
+  
+  #return ggplot object
+  return(gg_flow_plot);
+}
+
